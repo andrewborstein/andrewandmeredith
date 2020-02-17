31fc9a3f20c76318a4758c28f4f4eaff83452ff0
Andrew Borstein <andrew@andrewborstein.com>
2020-02-16 19:44:10 -0500

    Configure Netlify CMS to work locally
    - https://github.com/netlify/netlify-cms/issues/2335#issuecomment-580422779

--- src/_includes/partials/global/service-worker.js
+++ src/_includes/partials/global/service-worker.js
@@ -8,7 +8,7 @@ const EXCLUDED_URLS = [
   'admin',
   '.netlify',
   'https://identity.netlify.com/v1/netlify-identity-widget.js',
-  'https://unpkg.com/netlify-cms@^2.9.3/dist/netlify-cms.js'
+  'https://unpkg.com/netlify-cms@^2.10.17/dist/netlify-cms.js'
 ];
 
 // URLS that we want to be cached when the worker is installed
